621111c6e8e10471fe5e87e81d662942212a74ba
vanawy <saharchuk999@gmail.com>
2020-07-04 16:15:01 +0300

    Search by #hashtag and @username in inline mode

--- index.js
+++ index.js
@@ -4,16 +4,33 @@ const tt = require('tiktok-scraper');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const longUrl = /^https:\/\/www.tiktok.com\/@.+\/video\/\d+/i;
-const shortUrl = /^https:\/\/vm.tiktok.com\/.+\/?/i;
+function helpText(ctx) {
+    const bot = ctx.botInfo.username;
+    const text = `
+Send me a link to TikTok
+Commands:
+/help \\- for this message
+Inline mode \\(In chat with someone\\):
+\`@${bot} @username\` \\- find last posts from user
+\`@${bot} #hashtag\` \\- find last posts with hashtag
+\`@${bot} link\` \\- find video by link
+`;
+    ctx.reply(text, {
+        parse_mode: 'MarkdownV2'
+    })
+}
 
-bot.start((ctx) => ctx.reply('Send me a link to TikTok and I send video to you! (WITH WATERMARK)'));
-bot.help((ctx) => ctx.reply('Send me a link to TikTok'));
+bot.start(helpText);
+bot.help(helpText);
 bot.hears('hi', (ctx) => ctx.reply('Hello there!'))
 
 bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
 })
+
+
+const longUrl = /^https:\/\/www.tiktok.com\/@.+\/video\/\d+/i;
+const shortUrl = /^https:\/\/vm.tiktok.com\/.+\/?/i;
 bot.on('text', ctx => {
     const url = ctx.update.message.text;
     if (!url.match(longUrl) && !url.match(shortUrl)) {
@@ -36,30 +53,76 @@ bot.on('text', ctx => {
 
 
 bot.on('inline_query', ctx => {
-    const url = ctx.update.inline_query.query;
-    if (!url.match(longUrl) && !url.match(shortUrl)) {
-        ctx.answerInlineQuery([]);
+    const query = ctx.update.inline_query.query;
+    console.log(query);
+    if (query.match(longUrl) || query.match(shortUrl)) {
+        answerWithVideo(ctx, query);
         return;
     }
-    console.log(url);
-    tt.getVideoMeta(url)
+    if (query.startsWith("@")) {
+        const username = query.slice(1);
+        answerWithUserVideos(ctx, username);
+        return;
+    }
+    if (query.startsWith("#")) {
+        const hastag = query.slice(1);
+        answerWithHashtagVideos(ctx, hastag);
+        return;
+    }
+    ctx.answerInlineQuery([]);
+});
+
+function answerWithUserVideos(ctx, username) {
+    answerWithVideos(ctx, tt.user(username, { number: 10 }));
+}
+
+function answerWithHashtagVideos(ctx, hashtag) {
+    answerWithVideos(ctx, tt.hashtag(hashtag, { number: 10 }));
+}
+
+function answerWithVideos(ctx, data) {
+    data
+        .then(data => data.collector)
+        .then(videos => {
+            let result = [];
+            let id = 0;
+            for (let video of videos) {
+                const videoObject = createVideoObject(video, id++);
+                result.push(videoObject);
+            }
+            ctx.answerInlineQuery(result);
+        })
+        .catch(error => {
+            console.log(error);
+            ctx.answerInlineQuery([])
+        });
+}
+
+function answerWithVideo(ctx, url) {
+    return tt.getVideoMeta(url)
         .then(videoMeta => {
-            const title = `${videoMeta.text} | ${videoMeta.authorMeta.name}`;
-            const video = {
-                type: 'video',
-                id: 1,
-                mime_type: 'video/mp4',
-                thumb_url: videoMeta.imageUrl,
-                video_url: videoMeta.videoUrl,
-                caption: title,
-                title: title,
-            };
+            const video = createVideoObject(videoMeta);
             ctx.answerInlineQuery([video]);
         })
-        .catch(reason => {
-            console.log(reason);
+        .catch(error => {
+            console.log(error);
             ctx.answerInlineQuery([]);
         });
-});
+}
+
+function createVideoObject(video, id = 0) {
+    const title = `${video.text} | ${video.authorMeta.name}`;
+    const videoObject = {
+        type: 'video',
+        id: id,
+        mime_type: 'video/mp4',
+        thumb_url: video.covers.default,
+        video_url: video.videoUrl,
+        caption: title,
+        video_duration: video.videoMeta.duration,
+        title: title,
+    };
+    return videoObject;
+}
 
 bot.launch();
